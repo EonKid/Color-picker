104b793a80aa22c47a25a0a8d83824de27ec2e1e
Dhruv Narayan Singh <dhruvnarayan1023@gmail.com>
2015-11-21 16:58:48 +0530

    Color Picker

--- ColorPicker/ViewController.m
+++ ColorPicker/ViewController.m
@@ -9,19 +9,73 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (strong, nonatomic) IBOutlet UILabel *lblNothing;
+@property (strong, nonatomic) IBOutlet UIPickerView *viewPicker;
+@property (strong, nonatomic) NSArray *colorArray;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
+     self.colorArray  = [[NSArray alloc]         initWithObjects:@"Blue",@"Green",@"Orange",@"Purple",@"Red",@"Yellow" , nil];}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+// returns the number of 'columns' to display.
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
+}
+
+// returns the # of rows in each component..
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent: (NSInteger)component
+{
+    return 6;
+}
+
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row   forComponent:(NSInteger)component
+{
+    
+    return [self.colorArray objectAtIndex:row];
+    
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row   inComponent:(NSInteger)component{
+    
+    NSLog(@"Selected Row %d",(int) row);
+    switch(row)
+    {
+            
+        case 0:
+            self.lblNothing.text = @"Blue #0000FF";
+            self.lblNothing.textColor = [UIColor colorWithRed:0.0f/255.0f green: 0.0f/255.0f blue:255.0f/255.0f alpha:255.0f/255.0f];
+            break;
+        case 1:
+            self.lblNothing.text = @"Green #00FF00";
+            self.lblNothing.textColor = [UIColor colorWithRed:0.0f/255.0f green: 255.0f/255.0f blue:0.0f/255.0f alpha:255.0f/255.0f];
+            break;
+        case 2:
+            self.lblNothing.text = @"Orange #FF681F";
+            self.lblNothing.textColor = [UIColor colorWithRed:205.0f/255.0f green:   140.0f/255.0f blue:31.0f/255.0f alpha:255.0f/255.0f];
+            break;
+        case 3:
+            self.lblNothing.text = @"Purple #FF00FF";
+            self.lblNothing.textColor = [UIColor colorWithRed:255.0f/255.0f green:   0.0f/255.0f blue:255.0f/255.0f alpha:255.0f/255.0f];
+            break;
+        case 4:
+            self.lblNothing.text = @"Red #FF0000";
+            self.lblNothing.textColor = [UIColor colorWithRed:255.0f/255.0f green:   0.0f/255.0f blue:0.0f/255.0f alpha:255.0f/255.0f];
+            break;
+        case 5:
+            self.lblNothing.text = @"Yellow #FFFF00";
+            self.lblNothing.textColor = [UIColor colorWithRed:255.0f/255.0f green:   255.0f/255.0f blue:0.0f/255.0f alpha:255.0f/255.0f];
+            break;
+    }
+    
 }
 
 @end
